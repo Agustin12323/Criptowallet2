569d22698e116f907714961992b1211ab022bd45
ThomasLlopiz <toomillopiz@gmail.com>
2025-02-18 15:26:04 -0300

    borando comentarios

--- src/services/criptoYaService.js
+++ src/services/criptoYaService.js
@@ -1,18 +1,16 @@
-import axios from 'axios';
+import axios from "axios";
 
-// Configuración base para la API de CriptoYa
 const criptoYaApi = axios.create({
-  baseURL: 'https://criptoya.com/api',
-  timeout: 5000 // Tiempo límite para evitar bloqueos
+  baseURL: "https://criptoya.com/api",
+  timeout: 5000,
 });
 
-// Función para obtener el precio de una criptomoneda
 export const obtenerPrecio = async (exchange, cripto, moneda) => {
   try {
     const response = await criptoYaApi.get(`/${exchange}/${cripto}/${moneda}`);
-    return response.data; // La respuesta contiene el precio
+    return response.data;
   } catch (error) {
-    console.error('Error al obtener el precio:', error);
-    throw error; // Propaga el error para manejarlo en los componentes
+    console.error("Error al obtener el precio:", error);
+    throw error;
   }
 };
